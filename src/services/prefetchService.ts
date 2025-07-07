18849ced0042eeffabc5bd479356b5fcdfb3aa8e
Enuar Montaña <emontana@fgs.co>
2025-07-07 13:50:40 -0500

    chore: new env

--- src/services/prefetchService.ts
+++ src/services/prefetchService.ts
@@ -5,6 +5,10 @@ const getBaseUrl = () => {
   if (typeof window !== 'undefined') {
     return '';
   }  
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  }
+  
   return 'http://localhost:3000';
 };
 
